2b635aa877c3ce816faaa486603743e7fb723930
Artur-Petrov <petrov.artur02@gmail.com>
2026-05-04 16:00:29 +0300

    feature branch created finished part of reusable toggle component with all possible statuses

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Toggle from "./components/uiKit/toggle/Toggle.tsx";
+import {useState} from "react";
 
 function App() {
-
+    const [toggle, setToggle] = useState("Monthly");
   return (
     <>
-        <Toggle leftOption={'Monthly'} rightOption={'Annually'} activeOption={'Monthly'} onChange={}/>
+        <Toggle leftOption={'Monthly'} rightOption={'Annually'} activeOption={toggle} onChange={setToggle}/>
     </>
   )
 }
